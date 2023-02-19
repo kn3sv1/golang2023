30db6bfab538d06202d457e46d9e2c659052fdb1
RSatanovskyi <satcms@hotmail.com>
2023-02-19 15:33:07 +0200

    github go module path

--- 02_example/main.go
+++ 02_example/main.go
@@ -6,3 +6,6 @@ func main() {
 	name := "Go Developers2222"
 	fmt.Println("Azure for", name)
 }
+
+// https://metanit.com/go/tutorial/5.2.php
+// C:\Users\Envy\go\pkg\mod\github.com\inconshreveable\mousetrap@v1.0.0
